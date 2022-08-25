db79710bded267eb0701b4c3b080414a635a4953
Stammak93 <stamatis_makris93@yahoo.co.uk>
2022-08-25 19:39:36 +0100

    app refactor for heroku deployment

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoURI: "mongodb+srv://Stammak93:WJevbgTjkkmCc4uL@mongotest.o12w9.mongodb.net/MongoTest?retryWrites=true&w=majority",
+    googleClientID: "184497304812-94rpead4e4qu5b9ep1nafheugabblfnj.apps.googleusercontent.com",
+    googleClientSecret: "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",
+    cookieKey: "fasd76glF8YI6TOlld3hsakj4fafd56lhfdlhaRTIBBWfd"
+};
